a00ec3ad78dd1923fc8574da22df997c90f66251
Matthias Colin <matthias.colin@gmail.com>
2019-10-11 08:10:54 +0200

    begin web branch

--- src/main/java/cartes/JeuCarte.java
+++ src/main/java/cartes/JeuCarte.java
@@ -1,10 +1,32 @@
 package cartes;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class JeuCarte {
-	public static List<Carte> nouveauJeu() {
+	private List<Carte> cartes;
+	
+	public JeuCarte() {
+		this(false);
+	}
+	
+	public JeuCarte(boolean random) {
+		cartes = nouveauJeuAsList();
+		if (random) {
+			Collections.shuffle(cartes);
+		}
+	}
+	
+	public int taille() {
+		return cartes.size();
+	}
+	
+	public Carte retirer() {
+		return cartes.remove(0);
+	}
+	
+	public static List<Carte> nouveauJeuAsList() {
 		List<Carte> jeu = new LinkedList<>();
 		for (Signe signe: Signe.values()) {
 			for (Valeur valeur: Valeur.values()) {
---
+++ src/main/java/web/PokerServlet.java
@@ -0,0 +1,60 @@
+package web;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cartes.Carte;
+import cartes.JeuCarte;
+
+/**
+ * Servlet implementation class PokerServlet
+ */
+@WebServlet("/")
+public class PokerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PokerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * New game / reset or just go on
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		JeuCarte jeu = (JeuCarte) session.getAttribute("jeu");
+		boolean reset = Boolean.getBoolean(request.getParameter("reset"));
+		if (Objects.isNull(jeu)||reset) {
+			jeu = new JeuCarte(true);
+			session.setAttribute("jeu", jeu);
+		} else {
+			Carte carte = jeu.retirer();
+			request.setAttribute("carte", carte);
+		}
+		RequestDispatcher requestDispatcher = request
+                .getRequestDispatcher("/cartes.jsp");
+        requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
